84c6fa7b30bdb9c2356feded3c26e8a68cae16d1
sophiieschoen <105670811+sophiieschoen@users.noreply.github.com>
2023-01-25 13:40:11 -0800

    Update network_trainer.py
     self.max_num_epochs = 300
            self.num_batches_per_epoch = 50
            self.num_val_batches_per_epoch = 5
    save.every =10 (checkpoint)

--- nnunet/training/network_training/network_trainer.py
+++ nnunet/training/network_training/network_trainer.py
@@ -94,9 +94,9 @@ class NetworkTrainer(object):
         # too high the training will take forever
         self.train_loss_MA_alpha = 0.93  # alpha * old + (1-alpha) * new
         self.train_loss_MA_eps = 5e-4  # new MA must be at least this much better (smaller)
-        self.max_num_epochs = 1000
-        self.num_batches_per_epoch = 250
-        self.num_val_batches_per_epoch = 50
+        self.max_num_epochs = 300
+        self.num_batches_per_epoch = 50
+        self.num_val_batches_per_epoch = 5
         self.also_val_in_tr_mode = False
         self.lr_threshold = 1e-6  # the network will not terminate training if the lr is still above this threshold
 
@@ -119,7 +119,7 @@ class NetworkTrainer(object):
             self.use_progress_bar = bool(int(os.environ['nnunet_use_progress_bar']))
 
         ################# Settings for saving checkpoints ##################################
-        self.save_every = 50
+        self.save_every = 10
         self.save_latest_only = True  # if false it will not store/overwrite _latest but separate files each
         # time an intermediate checkpoint is created
         self.save_intermediate_checkpoints = True  # whether or not to save checkpoint_latest
